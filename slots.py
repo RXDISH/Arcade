7bad25f9d4f4f4c5b50f9194addebec6b528891c
Bri <165065075+RXDISH@users.noreply.github.com>
2024-11-26 16:17:37 +0000

    Slots V1

---
+++ slots.py
@@ -0,0 +1,26 @@
+import random
+
+print('Welcome to the slots!')
+print('')
+print('In order to win, you gotta line up a 777')
+print('Like this! 7️⃣ |7️⃣ |7️⃣')
+print('Good luck, you\'re gonna need it')
+print('')
+
+def play():
+  symbols = ['🍒', '🍇', '🍉', '7️⃣']
+
+  results = random.choices(symbols, k=3)
+  print(f'{results[0]} | {results[1]} | {results[2]}')
+  print('')
+
+  if (results[0] == '7️⃣' and results[1] == '7️⃣' and results[2] == '7️⃣'):
+    print('You win! :D')
+  else:
+    print('L bozo')
+    print('')
+
+answer = ''
+while answer.upper() != 'N':
+  play()
+  answer = input('Keep playing? (Y/N) ')
